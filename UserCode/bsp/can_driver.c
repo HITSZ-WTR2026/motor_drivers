1ee2d01abde17ee87a35c067e4e296a2c1745c0d
syhanjin <syhanjin666@gmail.com>
2025-10-01 20:19:22 +0800

    feat(bsp): implement CAN callback management system

--- UserCode/bsp/can_driver.c
+++ UserCode/bsp/can_driver.c
@@ -6,6 +6,18 @@
  */
 #include "can_driver.h"
 
+static CAN_CallbackMap maps[CAN_NUM];
+static size_t map_size = 0;
+
+static CAN_FifoReceiveCallback_t* get_callbacks(const CAN_HandleTypeDef* hcan)
+{
+    for (int i = 0; i < map_size; i++)
+        if (maps[i].hcan == hcan)
+            return maps[i].callbacks;
+
+    return NULL;
+}
+
 /**
  * 发送一条 CAN 消息
  * @param hcan can handle
@@ -46,4 +58,80 @@ void CAN_Start(CAN_HandleTypeDef* hcan, const uint32_t ActiveITs)
     {
         CAN_ERROR_HANDLER();
     }
+}
+
+/**
+ * 注册 CAN Fifo 处理回调
+ *
+ * @note 重复注册将会覆盖之前的回调
+ * @param hcan hcan
+ * @param filter_bank 注册对应的 filter 编号
+ * @param callback 回调函数指针
+ */
+void CAN_RegisterCallback(CAN_HandleTypeDef* hcan, const uint32_t filter_bank, CAN_FifoReceiveCallback_t callback)
+{
+    CAN_FifoReceiveCallback_t* callbacks = get_callbacks(hcan);
+
+    if (callbacks == NULL)
+    {
+        maps[map_size] = (CAN_CallbackMap){
+            .hcan      = hcan,
+            .callbacks = {NULL}};
+        callbacks = maps[map_size].callbacks;
+        map_size++;
+    }
+
+    callbacks[filter_bank] = callback;
+}
+
+/**
+ * 取消注册 CAN Fifo 处理回调
+ * @param hcan can handle
+ * @param filter_bank 需要取消注册对应的过滤器对应的 id
+ */
+void CAN_UnregisterCallback(CAN_HandleTypeDef* hcan, const uint32_t filter_bank)
+{
+    CAN_FifoReceiveCallback_t* callbacks = get_callbacks(hcan);
+    if (callbacks != NULL)
+        callbacks[filter_bank] = NULL;
+}
+
+/**
+ * CAN Fifo0 接收处理函数
+ *
+ * 本函数将会根据 hcan 和 rx_header 内部的 filter_id 来调用对应的回调函数
+ * @param hcan can handle
+ */
+void CAN_Fifo0ReceiveCallback(CAN_HandleTypeDef* hcan)
+{
+    CAN_RxHeaderTypeDef header;
+    uint8_t data[8];
+    if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &header, data) != HAL_OK)
+    {
+        CAN_ERROR_HANDLER();
+        return;
+    }
+    const CAN_FifoReceiveCallback_t* callbacks = get_callbacks(hcan);
+    if (callbacks != NULL && callbacks[header.FilterMatchIndex] != NULL)
+        callbacks[header.FilterMatchIndex](hcan, &header, data);
+}
+
+/**
+ * CAN Fifo1 接收处理函数
+ *
+ * 本函数将会根据 hcan 和 rx_header 内部的 filter_id 来调用对应的回调函数
+ * @param hcan can handle
+ */
+void CAN_Fifo1ReceiveCallback(CAN_HandleTypeDef* hcan)
+{
+    CAN_RxHeaderTypeDef header;
+    uint8_t data[8];
+    if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &header, data) != HAL_OK)
+    {
+        CAN_ERROR_HANDLER();
+        return;
+    }
+    const CAN_FifoReceiveCallback_t* callbacks = get_callbacks(hcan);
+    if (callbacks != NULL && callbacks[header.FilterMatchIndex] != NULL)
+        callbacks[header.FilterMatchIndex](hcan, &header, data);
 }
--- UserCode/bsp/can_driver.h
+++ UserCode/bsp/can_driver.h
@@ -12,8 +12,21 @@
 #include "main.h"
 
 #define CAN_ERROR_HANDLER() Error_Handler()
+#define CAN_NUM             (2)
+
+typedef void (*CAN_FifoReceiveCallback_t)(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef* header, uint8_t data[]);
+
+typedef struct
+{
+    CAN_HandleTypeDef* hcan;
+    CAN_FifoReceiveCallback_t callbacks[28];
+} CAN_CallbackMap;
 
 uint32_t CAN_SendMessage(CAN_HandleTypeDef* hcan, CAN_TxHeaderTypeDef header, uint8_t data[]);
 void CAN_Start(CAN_HandleTypeDef* hcan, uint32_t ActiveITs);
 
+void CAN_RegisterCallback(CAN_HandleTypeDef* hcan, uint32_t filter_bank, CAN_FifoReceiveCallback_t callback);
+void CAN_UnregisterCallback(CAN_HandleTypeDef* hcan, uint32_t filter_bank);
+void CAN_Fifo0ReceiveCallback(CAN_HandleTypeDef* hcan);
+void CAN_Fifo1ReceiveCallback(CAN_HandleTypeDef* hcan);
 #endif // CAN_H
